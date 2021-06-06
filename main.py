a88b69e3bdf4e55bfa94f5edb9b143f927977290
ShudarsanRegmi <shudarsanregmi555@gmail.com>
2021-06-06 11:12:51 +0545

    Minfor fixes

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#Online python3 code editor
-#Click on the "Run Code" button or press F10 key to run your code
-x = [1,2,3,4,5,6,7,8,9,10,11,12,14]
-for i in x:
-	print(i)
-print("a")
-print("b")
+import requests
+response = requests.get("https://www.guthib.com")
+print(response.text)
--- shell.php
+++ shell.php
@@ -4,7 +4,7 @@ if(isset($_POST["code"])) {
   $code = $_POST["code"];
   $file = fopen("main.py","w") or die("unable to open file");
   fwrite($file,$code);
-  $command = shell_exec("python3 main.py 2>&1");
-  echo htmlentities($command);
+  $response = shell_exec("python3 main.py 2>&1");
+  echo $response;
 }
  ?>
